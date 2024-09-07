353fb02e1aa3a2a25cfb15058cbe26e4e3fe636b
JosueELP <josueenrique1999@gmail.com>
2024-09-07 16:56:46 -0600

    Adding first draw of Projects page

--- src/app/components/projects-page.tsx
+++ src/app/components/projects-page.tsx
@@ -1,12 +1,25 @@
+"use client";
+
+import ProjectCard from '../components/project-card';
+import { useState, useEffect } from 'react'
 import styles from "../page.module.css";
 
 const classNames = require('classnames');
 
 export default function Projects() {
+  const [items, setItems] = useState(['1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7'])
+
   return (
     <div className={classNames(styles.main, styles.fontAiWritter)} id="projects">
-      <div>
-        <h3 className={classNames(styles.spaceDown)}>Some of my personal projects</h3>
+      <div className={classNames(styles.projectsContainer)}>
+        <div className={classNames(styles.spaceDown)}>
+          <h3 className={classNames(styles.spaceDown)}>Some of my personal projects</h3>
+        </div>
+        <div className={classNames(styles.projectsGrid)}>
+          {items.map((item, index) => (
+            <ProjectCard/> 
+          ))}
+        </div>
       </div>
     </div>
   );
