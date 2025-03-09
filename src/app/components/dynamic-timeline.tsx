3e4d86c74798eb5e45dcbe5dcb3bdf82e598d840
JosueELP <josueenrique1999@gmail.com>
2025-03-09 16:39:00 -0600

    [FIX]: Fixing compiling issue & fixing enterprise name

--- src/app/components/dynamic-timeline.tsx
+++ src/app/components/dynamic-timeline.tsx
@@ -10,7 +10,7 @@ interface TimelineItem {
   description: string
 }
 
-const constructTimeLineData = dictionary => {
+const constructTimeLineData = (dictionary : { [key: string]: string }) => {
   const initialTimelineData: TimelineItem[] = [
     {
       id: 1,
